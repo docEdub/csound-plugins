6feb11657cdc1160ce39a26b17e101ed27d09972
Andy Fillebrown <andy@docEdub.com>
2023-04-06 00:01:02 -0400

    Get sending string working from html to csound via websocket

--- websocket2/Websocket2.c
+++ websocket2/Websocket2.c
@@ -2,15 +2,16 @@
 
 #include <libwebsockets.h>
 
-static const size_t WriteBufferBytesCount = 2048;
+static const size_t WebsocketMessageCount = 1024;
 
  struct Websocket {
     struct lws_context *context;
     struct lws *websocket;
     struct lws_protocols *protocols;
     void *processThread;
-    unsigned char *messageBuffer;
     struct lws_context_creation_info info;
+    STRINGDAT messages[WebsocketMessageCount];
+    int messageCount;
     bool isRunning;
 };
 
@@ -27,47 +28,79 @@ static int32_t WS_callback(
 
     const struct lws_protocols *protocol = lws_get_protocol(websocket);
     WSget *p = protocol->user;
+    Websocket *ws = p->websocket;
     CSOUND *csound = p->csound;
     
+    if (inputData && 0 < inputDataSize) {
+        const int messageIndex = ws->messageCount % WebsocketMessageCount;
+        const int messageSize = inputDataSize + 1;
+
+        STRINGDAT *s = &ws->messages[messageIndex];
+        if (0 < s->size && s->size < messageSize) {
+            csound->Free(csound, s->data);
+            s->size = 0;
+        }
+        if (s->size == 0) {
+            s->size = sizeof(char) * messageSize;
+            s->data = csound->Calloc(csound, s->size);
+        }
+        memcpy(s->data, inputData, inputDataSize);
+        s->data[inputDataSize] = '\0';
+        csound->Message(csound, Str("%s\n"), s->data);
+
+        ws->messageCount++;
+    }
+
     return OK;
 }
 
 uintptr_t WS_processThread(void *vp)
 {
     WSget *p = vp;
-    p->websocket->isRunning = true;
+    Websocket *ws = p->websocket;
+    ws->isRunning = true;
 
     while (p->websocket->isRunning) {
+        ws->messageCount = 0;
 
-      // https://libwebsockets.org/lws-api-doc-main/html/group__service.html#gaf95bd0c663d6516a0c80047d9b1167a8
-      //
-      // Service any pending websocket activity.
-      //  context:  Websocket context
-      //  timeout_ms:  Set to 0; ignored; for backward compatibility
-      //
-      int returnValue = lws_service(p->websocket->context, 0);
+        // https://libwebsockets.org/lws-api-doc-main/html/group__service.html#gaf95bd0c663d6516a0c80047d9b1167a8
+        //
+        // Service any pending websocket activity.
+        //  context:  Websocket context
+        //  timeout_ms:  Set to 0; ignored; for backward compatibility
+        //
+        int returnValue = lws_service(p->websocket->context, 0);
 
-      // https://libwebsockets.org/lws-api-doc-main/html/group__callback-when-writeable.html#gabbe4655c7eeb3eb1671b2323ec6b3107
-      //
-      // Request a callback for all connections using the given protocol when it becomes possible to write to each socket without blocking in turn.
-      //  context:  Websocket context
-      //  protocol:  Protocol whose connections will get callbacks
-      lws_callback_on_writable_all_protocol(p->websocket->context, &p->websocket->protocols[0]);
+        // https://libwebsockets.org/lws-api-doc-main/html/group__callback-when-writeable.html#gabbe4655c7eeb3eb1671b2323ec6b3107
+        //
+        // Request a callback for all connections using the given protocol when it becomes possible to write to each socket without blocking in turn.
+        //  context:  Websocket context
+        //  protocol:  Protocol whose connections will get callbacks
+        lws_callback_on_writable_all_protocol(p->websocket->context, &p->websocket->protocols[0]);
     }
 
     return 0;
 }
 
-int32_t WS_deinitializeWebsocket(CSOUND *csound, void *vp)
+int32_t WS_destroyWebsocket(CSOUND *csound, void *vp)
 {
     WSget *p = vp;
-    p->websocket->isRunning = false;
-
     Websocket *ws = p->websocket;
+    ws->isRunning = false;
+
+    for (int i = 0; i < WebsocketMessageCount; i++) {
+        const STRINGDAT *s = &ws->messages[i];
+        if (s->data && 0 < s->size) {
+            csound->Free(csound, s->data);
+        }
+    }
+    ws->messageCount = 0;
+
     csound->JoinThread(ws->processThread);
+
     lws_cancel_service(ws->context);
     lws_context_destroy(ws->context);
-    csound->Free(csound, ws->messageBuffer);
+
     csound->Free(csound, ws->protocols);
     csound->Free(csound, ws);
 
@@ -97,18 +130,12 @@ Websocket *WS_createWebsocket(CSOUND *csound, const char *channelName, MYFLT por
 
     lws_set_log_level(LLL_DEBUG, NULL);
     ws->context = lws_create_context(&ws->info);
-    ws->messageBuffer = csound->Calloc(
-        csound,
-        LWS_SEND_BUFFER_PRE_PADDING +
-        (sizeof(char) * WriteBufferBytesCount) +
-        LWS_SEND_BUFFER_POST_PADDING
-    );
     if (UNLIKELY(ws->context == NULL)) {
         csound->Die(csound, "%s", Str("websocket: could not initialize websocket. Exiting"));
     }
 
     ws->processThread = csound->CreateThread(WS_processThread, p);
-    csound->RegisterDeinitCallback(csound, p, WS_deinitializeWebsocket);
+    csound->RegisterDeinitCallback(csound, p, WS_destroyWebsocket);
 
     return ws;
 }
